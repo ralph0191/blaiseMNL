d779c63e5310d013aa7f8303cc4cd1b646c128e2
rsuga <ralph.suga0191@gmai.com>
2019-03-21 17:17:48 +0800

    minor modification on seller page

--- sellerpage/includes/header.php
+++ sellerpage/includes/header.php
@@ -8,10 +8,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item">
-                    <h5 style="margin-right: 50px;  ">Welcome: Suga, Ralph Hide</h5>
+                    <h5 style="margin-right: 50px; margin-top:8px;  ">Welcome: Suga, Ralph Hide</h5>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link btn btn-blue" href='logout.php'>Logout</a>
+                    <a class="nav-link" href='logout.php'>Logout</a>
                 </li>
             </ul>
         </div>
