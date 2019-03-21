5db879743b9d9f5900775da690fb15d20ffa2a8a
rsuga <ralph.suga0191@gmai.com>
2019-03-21 15:20:36 +0800

    first commit

---
+++ sellerpage/includes/header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <h4>Seller Module</h4>
+        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-align-justify"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item">
+                    <h5 style="margin-right: 50px;  ">Welcome: Suga, Ralph Hide</h5>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-blue" href='logout.php'>Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
